e84c89fe75180a58eb2722fc7d4994ff3a40deaf
N0I0C0K <nick131410@aliyun.com>
2023-09-18 10:11:09 +0800

    doc：添加了帮助文档，添加了icon

--- syncl2r/command/app.py
+++ syncl2r/command/app.py
@@ -1,10 +1,34 @@
-import os
-import re
-
+import sys
 import typer
+from ..console import pprint
 
-app = typer.Typer(
-    name="Syncl2r",
-)
+help_text = r"""
+   _____                      __    ___    ____ 
+  / ___/ __  __ ____   _____ / /   |__ \  / __ \
+  \__ \ / / / // __ \ / ___// /    __/ / / /_/ /
+ ___/ // /_/ // / / // /__ / /___ / __/ / _, _/ 
+/____/ \__, //_/ /_/ \___//_____//____//_/ |_|  
+      /____/                                    
+"""
+
+description = """[red]SyncL2R[/] is an [yellow]all-in-one[/] tool for: 
+- [yellow]local and remote file synchronization
+- one-click deployment
+- and multiple functions.
+"""
+
+
+class Helper:
+    @property
+    def help(self):
+        if len(sys.argv) <= 1:
+            print(help_text)
+            pprint(description)
+        return ""
+
+
+helper = Helper()
+
+app = typer.Typer(name="Syncl2r", no_args_is_help=True, help=helper.help)
 
 __all__ = ["app"]
