20f9116cbff08874be5e7e53b8ad33f97ac11e75
N0I0C0K <nick131410@aliyun.com>
2023-12-01 19:10:09 +0800

    feat：实现远程配置获取

--- syncl2r/config/remote.py
+++ syncl2r/config/remote.py
@@ -1,24 +1,45 @@
 from .constant import Remote_Config_Data_Path
-from ..sync_core import RemoteFileManager
 from pydantic import BaseModel
 
 
 class RemoteConfig(BaseModel):
     pids: list[str]
 
-    def default():
+    @classmethod
+    def default(cls) -> "RemoteConfig":
         return RemoteConfig(pids=[])
 
 
-def load_remote_config(rfile: RemoteFileManager) -> RemoteConfig:
+__config: RemoteConfig | None = None
+
+
+def get_remote_config() -> RemoteConfig:
+    if __config is None:
+        global __config
+        __config = load_remote_config()
+    return __config
+
+
+def load_remote_config() -> RemoteConfig:
     from ..connect_core import get_global_connection
     from .local import get_global_config
 
     conn = get_global_connection()
     data = conn.utils.read_file(Remote_Config_Data_Path)
     if len(data) < 4:
-        return
+        return RemoteConfig.default()
+    import yaml
+
+    try:
+        from yaml import CLoader as Loader
+    except ImportError:
+        from yaml import Loader
+    obj = yaml.load(data, Loader)
+    return RemoteConfig.parse_obj(obj)
 
 
-def save_remote_config(config: RemoteConfig):
+def save_remote_config():
+    import yaml
+
+    yaml.dump(get_remote_config())
     pass
--- syncl2r/connect_core/utils.py
+++ syncl2r/connect_core/utils.py
@@ -2,16 +2,27 @@ from pathlib import PurePath
 from paramiko import SSHClient
 
 
+def path_to_absolute(path: PurePath) -> PurePath:
+    if not path.is_absolute():
+        from ..config import get_global_config
+
+        config = get_global_config()
+        path = PurePath(config.file_sync_config.remote_root_path) / path
+    return path
+
+
 class ConnectionFunction:
     def __init__(self, conn: SSHClient) -> None:
         self.conn = conn
 
     def read_file(self, path: PurePath) -> str:
-        if not path.is_absolute():
-            from ..config import get_global_config
-
-            config = get_global_config()
-            path = PurePath(config.file_sync_config.remote_root_path) / path
+        path = path_to_absolute(path)
 
         _, out, _ = self.conn.exec_command(f"cat {path.as_posix()}")
         return out.read().decode()
+
+    def write_file(self, path: PurePath, data: str):
+        from shlex import quote
+
+        path = path_to_absolute(path)
+        self.conn.exec_command(f'echo "{quote(data)}" > {path.as_posix}')
